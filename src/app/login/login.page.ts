11ddf3e4aa402da21cff3568a0839713204e6ac0
Jessica y Stiven <jsgaitan410@misena.edu.co>
2019-06-14 21:29:24 -0500

    Proyectoterminado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LoadingController } from '@ionic/angular';
-import { format } from 'util';
 
 @Component({
   selector: 'app-login',
@@ -25,14 +24,15 @@ User=
     {
         const loading = await this.loadingCtrl.create({
           message: 'Validando...',
-          spinner: 'bubbles'
+          spinner: 'bubbles',
+          duration:  2000,
         });
         loading.present();
         //this.User.correo = forma.value.email;
         //this.User.password = forma.value.clave; 
         console.log("ingrese a login"); 
     }
-    if (this.User.usuario === forma.value.user)
+    if (this.User.usuario === forma.value.nombre && this.User.password === forma.value.password  )
     {
       console.log("verdadero");
       //this.user.correo. = forma.value.email
